079baf63c4c4444c1a7bd486552c7691859a66ac
vikzh <vikt.anmail@gmail.com>
2019-11-30 19:10:32 +0300

    Error Handling added

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,3 +1,21 @@
 <?php
 
+namespace App;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(E_ALL);
+
+const ENV = 'local';
+
+$whoopsErrorHandler = new \Whoops\Run();
+if(ENV === 'local') {
+    $whoopsErrorHandler->prependHandler(new \Whoops\Handler\PrettyPageHandler());
+} else {
+    $whoopsErrorHandler->prependHandler(function ($e){
+        echo 'Error Page and Developer notification';
+    });
+}
+$whoopsErrorHandler->register();
+
 echo 'Main Page';
