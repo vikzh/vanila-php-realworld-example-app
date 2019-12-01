5cdd7ff65f65760fc6550c08eab157040631de8a
vikzh <vikt.anmail@gmail.com>
2019-12-02 02:18:13 +0300

    Routing added

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,6 +4,13 @@ namespace App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Controllers\ArticleController;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpFoundation\Request;
+
 error_reporting(E_ALL);
 
 const ENV = 'local';
@@ -18,7 +25,24 @@ if(ENV === 'local') {
 }
 $whoopsErrorHandler->register();
 
-$request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$route1 = new Route('/articles', ['_controller' => ArticleController::class]);
+$route2 = new Route('/articles/{slug}', ['_controller' => ArticleController::class]);
+$routes = new RouteCollection();
+$routes->add('index', $route1);
+$routes->add('show', $route2);
+
+$requestContext = new RequestContext();
+$requestContext->fromRequest(Request::createFromGlobals());
+
+$request = Request::createFromGlobals();
+
+$matcher = new UrlMatcher($routes, $requestContext);
+$routeInfo = $matcher->matchRequest($request);
+
+$controller = new $routeInfo['_controller']();
+$method = $routeInfo['_route'];
+$controller->$method();
+
 $response = new \Symfony\Component\HttpFoundation\Response();
 $response->setContent('Test text');
 $response->prepare($request);
---
+++ src/Controllers/ArticleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class ArticleController
+{
+    public  function index()
+    {
+        echo 'Article Controller index method';
+    }
+
+    public function show()
+    {
+        echo 'Article Controller show method';
+    }
+}
