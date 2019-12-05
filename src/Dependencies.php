1fbfd0c86557a1e86018b813254c13eec1c1ea57
vikzh <vikt.anmail@gmail.com>
2019-12-05 15:42:37 +0300

    dependencies moved to another dir

---
+++ src/Dependencies.php
@@ -0,0 +1,20 @@
+<?php
+
+use DI\Container;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use App\Template\Renderer;
+use function DI\create;
+
+$diContainer = new Container();
+$diContainer->set(Request::class, Request::createFromGlobals());
+$diContainer->set(Response::class, new Response());
+$diContainer->set(Environment::class, function (){
+   $loader = new FilesystemLoader(__DIR__ . '/../templates');
+   return new Environment($loader);
+});
+$diContainer->set(Renderer::class, create(Renderer::class)->constructor(\DI\get(Environment::class)));
+
+return $diContainer;
