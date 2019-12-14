015192479903ee891afa21b17672becba2ae9fa2
vikzh <vikt.anmail@gmail.com>
2019-12-14 15:55:38 +0300

    User migration updated

---
+++ database/migrations/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Migrations;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+Capsule::schema()->create('users', function ($table) {
+    $table->increments('id');
+    $table->string('username')->unique();
+    $table->string('email')->unique();
+    $table->string('password');
+    $table->string('bio');
+    $table->string('image');
+    $table->timestamps();
+});
