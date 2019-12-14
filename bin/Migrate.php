082220a16838654f285a6e84b7fc923e637a5950
vikzh <vikt.anmail@gmail.com>
2019-12-14 17:40:56 +0300

    CLI migrate bootstrap removed

--- bin/Migrate.php
+++ bin/Migrate.php
@@ -1,5 +1,9 @@
 <?php
 
-include_once __DIR__ . '/../src/Bootstrap.php';
+$_SERVER['PATH_INFO'] = '/migrate';
+
+require __DIR__ . '/../vendor/autoload.php';
+include_once __DIR__ . '/../src/Database.php';
 
 include_once __DIR__ . '/../database/migrations/Article.php';
+include_once __DIR__ . '/../database/migrations/User.php';
