3d93c27b43d9aec7586d9de328c2433d9d4d74f0
vikzh <vikt.anmail@gmail.com>
2019-12-25 12:24:24 +0300

    article migration updated

--- database/migrations/Article.php
+++ database/migrations/Article.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 Capsule::schema()->create('articles', function ($table) {
     $table->increments('id');
-    $table->string('slug');
+    $table->integer('user_id');
+    $table->string('slug')->unique();
     $table->string('title');
     $table->string('description');
     $table->text('body');
     $table->timestamps();
+
+    $table->foreign('user_id')
+        ->references('id')
+        ->on('users')
+        ->onDelete('cascade');
 });
